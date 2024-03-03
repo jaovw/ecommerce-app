7c6519c38533e92ae3ed24961f3910e911292993
jaovw <joojvw@gmail.com>
2024-03-03 01:25:22 -0300

    - Navbar - removendo conteudo padrão

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-betwenn z-50 bg-slate-800 text-gray-300">
+      <Link
+        className="uppercase font-bold text-md h-12 flex items-center"
+        rel="stylesheet"
+        href="/"
+      >
+        E-commerce
+      </Link>
+    </nav>
+  );
+}
